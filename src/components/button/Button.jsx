132353765a8832db993b780fce929feac6ff8df8
Bui long <longbh.bof@cenhomes.vn>
2022-11-02 14:56:45 +0700

    fix css

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Button.scss';
+
+const Button = () => {
+    return (
+        <div>
+            <button className="btn-click">Click me!</button>
+        </div>
+    );
+};
+
+export default Button;
